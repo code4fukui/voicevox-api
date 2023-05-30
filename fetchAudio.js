c316e57c02fdb824f56ef09420653982d724ca51
Taisuke Fukuno <fukuno@jig.jp>
2023-05-30 14:46:12 +0900

    add param endpoint

--- fetchAudio.js
+++ fetchAudio.js
@@ -1,16 +1,16 @@
 import { fetchJSON } from "https://js.sabae.cc/fetchJSON.js";
 import { fetchBinByJSON } from "https://js.sabae.cc/fetchBinByJSON.js";
 
-export const fetchAudio = async (text, speaker = 0) => {
+export const fetchAudio = async (text, speaker = 0, endpoint = "http://localhost:50031") => {
   const req = {
     text,
     speaker,
   };
-  const urlpre = `http://localhost:50031/audio_query?text=${encodeURIComponent(text)}&speaker=${speaker}`;
+  const urlpre = endpoint + `/audio_query?text=${encodeURIComponent(text)}&speaker=${speaker}&core_version=0.0.0`;
   const json = await fetchJSON(urlpre, req);
-  //console.log(json);
+  console.log(json);
   
-  const url = "http://localhost:50031/synthesis";
+  const url = endpoint + "/synthesis";
   const bin = await fetchBinByJSON(`${url}?speaker=${speaker}&core_version=0.0.0&enable_interrogative_upspeak=true`, json);
   return bin;
 };
--- say.js
+++ say.js
@@ -1,12 +1,14 @@
 import { fetchAudio } from "./fetchAudio.js";
 
 if (Deno.args.length == 0) {
-  console.log("deno run -A say.js [text] (speakerid)");
+  console.log("deno run -A say.js [text] (speakerid) (endpoint)");
   Deno.exit(1);
 }
 
 const text = Deno.args[0];
 const speakerid = Deno.args[1] || 0;
+const endpoint = Deno.args[2] || undefined;
 
-const bin = await fetchAudio(text, speakerid);
+console.log(text);
+const bin = await fetchAudio(text, speakerid, endpoint);
 await Deno.writeFile("out.wav", bin);
